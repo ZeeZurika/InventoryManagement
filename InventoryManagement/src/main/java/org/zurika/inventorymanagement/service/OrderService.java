19316cda16a87852e193e6e6ea18f2d453f4be0d
ZeeZurika <117533446+ZeeZurika@users.noreply.github.com>
2025-01-07 03:38:48 -0500

    the service package is added to the project

--- InventoryManagement/src/main/java/org/zurika/inventorymanagement/service/OrderService.java
+++ InventoryManagement/src/main/java/org/zurika/inventorymanagement/service/OrderService.java
@@ -1,10 +1,34 @@
 package org.zurika.inventorymanagement.service;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.zurika.inventorymanagement.model.Order;
+import org.zurika.inventorymanagement.repository.OrderRepository;
 
 @Service
 @Transactional
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> findById(Long id){
+        return orderRepository.findById(id);
+    }
+
+    public Order save(Order order){
+        return orderRepository.save(order);
+    }
+
+    public void deleteById(Long id){
+        orderRepository.deleteById(id);
+    }
 
 }
--- InventoryManagement/src/main/java/org/zurika/inventorymanagement/service/SupplierService.java
+++ InventoryManagement/src/main/java/org/zurika/inventorymanagement/service/SupplierService.java
@@ -1,10 +1,33 @@
 package org.zurika.inventorymanagement.service;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.zurika.inventorymanagement.model.Supplier;
+import org.zurika.inventorymanagement.repository.SupplierRepository;
 
 @Service
 @Transactional
 public class SupplierService {
 
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public List<Supplier> findAll(){
+        return supplierRepository.findAll();
+    }
+
+    public Optional<Supplier> findById(Long id){
+        return supplierRepository.findById(id);
+    }
+
+    public Supplier save(Supplier supplier){
+        return supplierRepository.save(supplier);
+    }
+
+    public void deleteById(Long id){
+        supplierRepository.deleteById(id);
+    }
 }
